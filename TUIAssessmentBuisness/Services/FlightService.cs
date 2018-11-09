0e71cc38df4fe7b31b056644e14b17857905fb84
Alexis CHOLLET-EXT <alexis.chollet-ext@fnacdarty.com>
2018-11-09 14:49:56 +0100

    Ajout d'une extension ToRadians()

--- TUIAssessmentBuisness/Services/FlightService.cs
+++ TUIAssessmentBuisness/Services/FlightService.cs
@@ -34,18 +34,38 @@ namespace TUIAssessmentBuisness.Services
 
         private double HaversineFormula(CoordinatesModel coordinates1, CoordinatesModel coordinates2)
         {
-            var deltaLambda = Math.Abs(DegreeToRadian(coordinates1.Longitude) - DegreeToRadian(coordinates2.Longitude));
-            var deltaPhi = Math.Abs(DegreeToRadian(coordinates1.Latitude) - DegreeToRadian(coordinates2.Latitude));
+            var deltaLambda = Math.Abs(coordinates1.Longitude.ToRadians() - coordinates2.Longitude.ToRadians());
+            var deltaPhi = Math.Abs(coordinates1.Latitude.ToRadians() - coordinates2.Latitude.ToRadians());
 
-            var deltaSigma = Math.Asin(Math.Sqrt(Math.Pow(Math.Sin(deltaPhi / 2), 2) + Math.Cos(DegreeToRadian(coordinates1.Latitude)) * Math.Cos(DegreeToRadian(coordinates2.Latitude)) * Math.Pow(Math.Sin(deltaLambda / 2), 2)));
+            var deltaSigma = Math.Asin(Math.Sqrt(Math.Pow(Math.Sin(deltaPhi / 2), 2) + Math.Cos(coordinates1.Latitude.ToRadians()) * Math.Cos(coordinates2.Latitude.ToRadians()) * Math.Pow(Math.Sin(deltaLambda / 2), 2)));
 
             return Math.Round(2 * _earthRadius * deltaSigma, 2);
         }
 
-        private double DegreeToRadian(double angle)
+        private double VicentyFormulae(CoordinatesModel coordinates1, CoordinatesModel coordinates2)
         {
-            return (Math.PI / 180.0) * angle;
-        }
+            var deltaLambda = Math.Abs(coordinates1.Longitude.ToRadians() - coordinates2.Longitude.ToRadians());
 
+            var numerator = Math.Sqrt(Math.Pow(Math.Cos(coordinates2.Latitude.ToRadians()) * Math.Sin(deltaLambda), 2) + Math.Pow(Math.Cos(coordinates1.Latitude.ToRadians()) * Math.Sin(coordinates2.Latitude.ToRadians() - Math.Sin(coordinates1.Latitude.ToRadians() * Math.Cos(coordinates2.Latitude.ToRadians()) * Math.Cos(deltaLambda))), 2));
+
+            var denominator = Math.Sin(coordinates1.Latitude.ToRadians()) * Math.Sin(coordinates2.Latitude.ToRadians()) + Math.Cos(coordinates1.Latitude.ToRadians()) * Math.Cos(coordinates2.Latitude.ToRadians()) * Math.Cos(deltaLambda);
+
+            var deltaSigma = Math.Atan(numerator / denominator);
+
+            return Math.Round(2 * _earthRadius * deltaSigma, 2);
+        }
+    }
+
+    /// <summary>
+    /// Convert to Radians.
+    /// </summary>
+    /// <param name="val">The value to convert to radians</param>
+    /// <returns>The value in radians</returns>
+    public static class NumericExtensions
+    {
+        public static double ToRadians(this double val)
+        {
+            return (Math.PI / 180) * val;
+        }
     }
 }
